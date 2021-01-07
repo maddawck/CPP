c68bcb605cc135e63468267873708088da9c9e14
Sounak Ghosh <sounakghosh.official@gmail.com>
2021-01-07 21:46:13 +0530

    Create Day 1- Area of a Triangle (no input).cpp

---
+++ Day 1- Area of a Triangle (no input).cpp
@@ -0,0 +1,20 @@
+//program to calculate the area of a right angled triangle where the sides are already given
+
+#include <iostream>
+
+using namespace std
+
+int main()
+{
+  int height, base;
+  double area;
+  
+  height=4,base=5;
+  
+  area=(base*height)/2;
+  
+  cout<<"Area of a triangle is "<<area;
+  
+  return 0;
+  
+}

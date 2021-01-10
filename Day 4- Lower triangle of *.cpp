58a9f07e343bf7250b0da70f83f6db52f55c5296
Sounak Ghosh <sounakghosh.official@gmail.com>
2021-01-10 21:50:19 +0530

    Create Day 4- Lower triangle of *.cpp

---
+++ Day 4- Lower triangle of *.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void Draw(int n)
+{
+    
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    int a;
+    cout<<"Enter the number of rows\n";
+    cin>>a;
+    Draw(a);
+
+    return 0;
+}

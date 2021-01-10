ec45c72ba162dc2be06e7e5229ae7d726ad2629f
Sounak Ghosh <sounakghosh.official@gmail.com>
2021-01-10 21:30:25 +0530

    Create Day 3- Reverse a Number.cpp

---
+++ Day 3- Reverse a Number.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void Reverse(int n)
+{
+    int rev=0;
+    int m=n;
+    int r;
+    
+    while(n>0)
+    {
+        r=n%10;
+        n=n/10;
+        rev=rev*10+r;
+    }
+    cout<<rev;
+    
+}
+int main()
+{
+    int a;
+    cout<<"Enter the number\n";
+    cin>>a;
+    Reverse(a);
+
+    return 0;
+}

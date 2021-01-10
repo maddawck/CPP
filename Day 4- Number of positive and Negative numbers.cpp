7af6ab943b71add0106079617046617c1c83907e
Sounak Ghosh <sounakghosh.official@gmail.com>
2021-01-10 21:37:09 +0530

    Create Day 4- Number of positive and Negative numbers.cpp

---
+++ Day 4- Number of positive and Negative numbers.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void Count()
+{
+    int A[]={3,5,-2,9,-4,10,-24,19,81,-7,12,13};
+    int pcount=0;
+    int ncount=0;
+    
+    for(int x:A)
+    {
+        if(x<0)
+            ncount++;
+        else
+            pcount++;
+    }
+    
+    cout<<pcount<<" "<<ncount;
+}
+int main()
+{
+    
+    Count();
+
+    return 0;
+}

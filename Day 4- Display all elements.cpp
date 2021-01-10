8efcce4dc708b42982ec0e75732e9aed185405f0
Sounak Ghosh <sounakghosh.official@gmail.com>
2021-01-10 21:33:01 +0530

    Create Day 4- Display all elements.cpp

---
+++ Day 4- Display all elements.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void Display()
+{
+    int A[]={2,4,6,8,10};
+    
+    
+    for(int x: A)
+    {
+        cout<<x<<" ";
+    }
+
+    
+}
+int main()
+{
+    Display();
+
+    return 0;
+}

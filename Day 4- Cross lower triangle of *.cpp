75999d6e81a124d15765999ee3a460b21a169a91
Sounak Ghosh <sounakghosh.official@gmail.com>
2021-01-10 21:54:55 +0530

    Create Day 4- Cross lower triangle of *.cpp

---
+++ Day 4- Cross lower triangle of *.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void Draw(int n)
+{
+    
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(i+j>=n-1)
+                cout<<"* ";
+            else
+                cout<<"  ";
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    int a;
+    cout<<"Enter the number of rows\n";
+    cin>>a;
+    Draw(a);
+
+    return 0;
+}

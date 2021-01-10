416a9e1140b4f26b7a4e12f14efee635dea142d8
Sounak Ghosh <sounakghosh.official@gmail.com>
2021-01-10 21:26:44 +0530

    Create Day 3- Display digits.cpp

---
+++ Day 3- Display digits.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void Digits(int n)
+{
+    int digit;
+    
+    while(n>0)
+    {
+        digit=n%10;
+        n=n/10;
+        cout<<digit<<" ";
+    }
+}
+int main()
+{
+    int a;
+    cout<<"Enter the number\n";
+    cin>>a;
+    Digits(n);
+
+    return 0;
+}

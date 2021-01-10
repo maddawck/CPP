78dcf9166037b652dcfa7718983997ded9af0436
Sounak Ghosh <sounakghosh.official@gmail.com>
2021-01-10 14:08:02 +0530

    Create Day 3- Discounted price.cpp

---
+++ Day 3- Discounted price.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+void Discount(int amount)
+{
+    float disAmount;
+    
+    if(amount>=5000)
+    {
+        disAmount=amount-amount*20/100.0;
+        cout<<disAmount;
+    }
+    else
+    {
+        if(amount>=2000 && amount<5000)
+        {
+            disAmount=amount-amount*10/100.0;
+            cout<<disAmount;
+        }
+        else
+        {
+            disAmount=amount-amount*5/100.0;
+            cout<<disAmount;
+        }
+    }
+}
+
+int main()
+{
+    int a;
+    cout<<"Enter the amount\n";
+    cin>>a;
+    Discount(a);
+
+    return 0;
+}
